e300858d910d28f610884b3f042b986b29e4af30
Noah Hopkins <noah.mich.hopk@gmail.com>
2023-03-24 10:57:34 -0400

    Add project files.

---
+++ Topic 6 Pt 2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Topic_6_Pt_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double passingScore = 0.00;
+            double failingScore = 0.00;
+            double score, percentageOfPassingScores;
+            int i = 0;
+            string input;
+            
+            Console.WriteLine("Enter exit if you are done entering scores");
+
+            while (i == 0)
+            {
+                Console.WriteLine("Enter a score");
+                input = Console.ReadLine();
+
+                if (Double.TryParse(input, out score))
+                {
+                    if (score > 70 && score < 100)
+                    {
+                        passingScore++;
+
+                    }
+                    else if (score > 100)
+                    {
+                        Console.WriteLine("Invalid score. Please try again.");
+                    }
+                    else if (score < 70)
+                    {
+                        failingScore++;
+                    }
+                }
+                else if (input.ToLower() == "exit")
+                {
+                    i = 1;
+                    percentageOfPassingScores = passingScore / (passingScore + failingScore);
+                    Console.WriteLine(percentageOfPassingScores);
+                    Console.WriteLine(percentageOfPassingScores + "% of the scores are passing");
+                }            
+            }       
+        }
+    }
+}
